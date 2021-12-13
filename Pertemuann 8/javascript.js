9bf5546575320be3dfbca910a8b00387ab32b668
gilanggfzn <91368332+gilanggfzn@users.noreply.github.com>
2021-12-13 16:17:40 +0700

    Add files via upload

---
+++ Pertemuann 8/javascript.js
@@ -0,0 +1,30 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) // kalo lebih dari 3 balik ke 1
+  {slideIndex = 1}
+  if (n < 1) // kalo kurang dari 1 balik ke 3
+  {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
